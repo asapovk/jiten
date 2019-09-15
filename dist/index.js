65a6b6a4fffded71b67db172546d96cb4b29cade
asapovk <asapovk@gmail.com>
2019-09-15 19:46:00 +0300

    added create word api

--- dist/index.js
+++ dist/index.js
@@ -122,6 +122,34 @@ app.listen({ port }, () => {
         });
         res.send(words);
     });
+    app.post('/api/create/word', async (req, res) => {
+        const writing = req.body.writing;
+        const translation = req.body.translation ? req.body.translation : '';
+        const hiragana = req.body.hiragana ? req.body.hiragana : '';
+        const romaji = req.body.romaji ? req.body.romaji : '';
+        const author = req.body.author ? req.body.author : '';
+        const origin = req.body.origin ? req.body.origin : '';
+        const usage = req.body.usage ? req.body.usage : '';
+        const time = Date.now().toString();
+        const createdWord = await index_1.prisma.createWord({
+            writing: writing,
+            hiragana: hiragana,
+            romaji: romaji,
+            translation: {
+                set: [translation]
+            },
+            updateHistory: {
+                create: {
+                    origin,
+                    usage,
+                    newTranslation: translation,
+                    time,
+                    author
+                }
+            }
+        });
+        res.send(createdWord);
+    });
     app.put('/api/word', async (req, res) => {
         const writing = req.body.writing;
         const translation = req.body.translation ? req.body.translation : '';
